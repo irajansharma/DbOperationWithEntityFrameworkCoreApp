8bf0008eb02a5709694fc634148bb330cb2ebf1d
irajansharma <rajanlamsal722@gmail.com>
2026-02-07 00:58:05 +0545

    Fixed the build failed operation and Added other tables with relationship

---
+++ DbOperationWithEntityFrameworkCoreApp/Data/BookPrice.cs
@@ -0,0 +1,15 @@
+﻿namespace DbOperationWithEntityFrameworkCoreApp.Data
+{
+    public class BookPrice
+    {
+        public int Id { get; set; }
+        public int BookId { get; set; }
+        public int CurrencyId { get; set; }
+        public int Amount { get; set; }
+
+        public  Book Book { get; set; }
+        public  Currency Currency { get; set; }
+    }
+
+}
+
