2463ed7d9de84707ff4650976dcdf421ed40d1ef
irajansharma <rajanlamsal722@gmail.com>
2026-02-06 23:44:03 +0545

    Added all the required packages for making connect to database

--- DbOperationWithEntityFrameworkCoreApp/Program.cs
+++ DbOperationWithEntityFrameworkCoreApp/Program.cs
@@ -1,4 +1,10 @@
+using DbOperationWithEntityFrameworkCoreApp.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("Database")));
 
 // Add services to the container.
 
